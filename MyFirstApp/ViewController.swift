949c853e293e0596d26fefe40eedc2ea6c49e58d
Chang Louise <changlouise@changde-macbook.local>
2018-10-30 20:25:42 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Chang Louise on 2018/10/30.
+//  Copyright © 2018年 Chang Louise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
